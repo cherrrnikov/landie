a53cba24da7d42939217c3ca20a57488536830ac
cherrrnikov <chernikoviam@gmail.com>
2020-11-08 23:01:49 +0300

    create slider without adaptive

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  var infoSlider = new Swiper('.info-slider', {
+  speed: 900,
+  autoplay: {
+    enabled: true,
+    delay: 5000, 
+  },
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Подключение клавиатуры
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  })
+  $(".info-slider").on("mouseenter", function() {
+    infoSlider.autoplay.stop();
+  });
+  $(".info-slider").on("mouseleave", function() {
+    infoSlider.autoplay.start();
+  });
+})
